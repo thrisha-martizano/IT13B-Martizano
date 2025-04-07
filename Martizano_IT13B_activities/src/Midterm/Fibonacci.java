de428093b0ae8de5b8326f562df9c0afdf8f89fa
LutragoacliffordIT24A <martizanothrisha@gmail.com>
2025-04-07 11:43:40 +0800

    Added Fibonacci Act.

--- Martizano_IT13B_activities/src/Midterm/Fibonacci.java
+++ Martizano_IT13B_activities/src/Midterm/Fibonacci.java
@@ -12,19 +12,17 @@ public class Fibonacci {
 
     public static void main(String[] args) {
         int n = 10;
-        System.out.println("result: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 ");
-        for (int f = 10; f < n; f++) {
-            System.out.print((f) + " , ");
-
+        System.out.print("fibonacci sequence of " + n + ": ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci(i) + ",");
         }
     }
 
     public static int fibonacci(int n) {
-        if (n >= 1) {
+        if (n <= 1) {
             return n;
         } else {
-            return fibonacci(n - 1 + n - 2);
-
+            return fibonacci(n - 1) + fibonacci(n - 2);
         }
     }
 }
